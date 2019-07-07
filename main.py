e32d5e2ef5f4362359d200528327f6d6909175a7
Spencer Axford <saxford@mun.ca>
2019-07-07 19:08:45 -0230

    Set up image processing for IAU database images

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import cv2
+import numpy as np
 
 def main():
-    img = cv2.imread('DemoImages/big_dipper.jpg', cv2.IMREAD_GRAYSCALE)
-    thresh = 127
+    # Read in Image
+    img = cv2.imread('DemoImages/UrsaMajor.jpg', cv2.IMREAD_COLOR)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+   
+    #Threshold to binary
+    thresh = 100
     img = cv2.threshold(img, thresh, 255, cv2.THRESH_BINARY)[1]  
+
+    #Isolated unwanted text elements
+    text = cv2.threshold(gray, 170, 255, cv2.THRESH_BINARY)[1]  
+    dilated = cv2.dilate(text, np.ones((11, 11)))
+    
+    #Remove text elements leaving only stars in red and lines in blue
+    img[dilated > 250] = 0
+    
+    #Show image
     cv2.imshow('image', img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-
 
 if __name__ == "__main__":
     main()
