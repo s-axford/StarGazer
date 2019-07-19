acbe5cb8c327f5b0fd70ec54e72cbc81b7e6faa5
timothy <tim.mather@fytics.com>
2019-07-19 11:39:05 -0230

    Format and straighten lines

--- constellation.py
+++ constellation.py
@@ -1,10 +1,14 @@
-from helper import straighten
+from helper import straighten, straighten_lines
+
+
 class Constellation:
     stars_x = []
     stars_y = []
     stars_mags = []
     lines = []
+    rotation_angle = None
     number_of_stars = 0
+    brightest_star = ()  # I need this in (x, y) format of the position of the brightest star for moving the lines
 
     def __init__(self, x, y, mag, cons_lines):
         self.stars_x = x
@@ -20,4 +24,5 @@ class Constellation:
         print(self.lines)
 
     def straighten(self):
-        self.stars_x, self.stars_y = straighten(self.stars_x, self.stars_y, self.stars_mags)
+        self.stars_x, self.stars_y, self.rotation_angle = straighten(self.stars_x, self.stars_y, self.stars_mags)
+        self.lines = straighten_lines(self.lines, self.rotation_angle)
--- helper.py
+++ helper.py
@@ -10,9 +10,47 @@ def straighten(x, y, mags):
     dx = x[l2] - x[l1]
     dy = y[l2] - y[l1]
     angle = 90*(1-np.sign(dx)) + math.atan(dy/dx)
-    rotation_matrix = np.array([[math.cos(angle), math.sin(angle)],[-math.sin(angle), math.cos(angle)]])
+    rotation_matrix = np.array([[math.cos(angle), math.sin(angle)], [-math.sin(angle), math.cos(angle)]])
     position_matrix = np.array([x, y])
     result = np.matmul(rotation_matrix, position_matrix)
     x = result[0]
     y = result[1]
-    return x,y
+    return x, y, angle
+
+
+# formats lines to x and y lists
+def format_lines(lines):
+    result_x = []
+    result_y = []
+    for i in lines:
+        for x1, y1, x2, y2 in i:
+            result_x.append(x1)
+            result_x.append(x2)
+            result_y.append(y1)
+            result_y.append(y2)
+    return np.array([result_x, result_y])
+
+
+# reverts format_lines
+def reformat_lines(x, y):
+    lines = []
+    i = 0
+    row = []
+    for item in range(len(x)):
+        row.append(x[item])
+        row.append(y[item])
+        i += 1
+        if i == 2:
+            i = 0
+            lines.append([row])
+            row = []
+    return np.array(lines)
+
+
+# straightens lines to match stars
+def straighten_lines(lines, angle):
+    formatted_lines = format_lines(lines)
+    result = np.matmul(np.array([[math.cos(angle), -math.sin(angle)], [math.sin(angle), math.cos(angle)]]),
+                       formatted_lines)
+    x = reformat_lines(result[0], result[1])
+    return x
